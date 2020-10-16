145a0c00c68b1a60a9db052dd2b3402e5f2b8721
anarquia17 <jorgdavillara@gmail.com>
2020-10-16 01:17:19 -0500

    feat: add Card docentes tercera version

--- src/pages/teachers.js
+++ src/pages/teachers.js
@@ -1,11 +1,27 @@
 import React from "react"
 import Header from "../components/header/header"
 import Carrusele from "../components/carrusel/carrusele"
+import Card_Teacher from "../components/card_teacher/card_teacher"
 export default function teachers() {
   return (
     <div >
         <Header />
         <Carrusele />
+
+        <div className="teacher-container">
+        <Card_Teacher
+          name="Juan Perez"
+          description= "Docente del curso de Angular React Vuejs"
+        />
+        <Card_Teacher
+          name="Vickie Schamberger"
+          description= "Docente del curso de Angular React Vuejs"
+        />
+        <Card_Teacher
+          name="Gage Waelchi"
+          description= "Docente del curso de Angular React Vuejs"
+        />
+        </div>
     </div>
   )
 }
