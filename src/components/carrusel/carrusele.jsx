25f545a0f31eccb3680690d6150b058785d59fea
anarquia17 <jorgdavillara@gmail.com>
2020-10-16 00:50:16 -0500

    fix: add Carrusel segunda version

--- src/components/carrusel/carrusele.jsx
+++ src/components/carrusel/carrusele.jsx
@@ -1,16 +1,17 @@
 import React from "react"
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./carrusele.css"
 import Carousel from "react-bootstrap/Carousel"
 import { CarouselItem } from "react-bootstrap"
 import slide01 from "../../img/Banner1.jpg"
 import slide02 from "../../img/Banner2.jpg"
 import slide03 from "../../img/Banner3.jpg"
+import slide04 from "../../img/Banner2.jpg"
 
 export default function Carrusele() {
   return (
-    <session className="banner">
-      <div>
+  
+      <div className="content">
         <Carousel>
           <CarouselItem> 
             <img className="h-25 d-inline-block  d-block w-100" src={slide01} alt="Primer Banner" />
@@ -23,8 +24,12 @@ export default function Carrusele() {
           <CarouselItem>
             <img className="h-25 d-inline-block d-block w-100" src={slide03} alt="Tercer Banner" />
           </CarouselItem>
+
+          <CarouselItem>
+            <img className="h-25 d-inline-block d-block w-100" src={slide04} alt="Tercer Banner" />
+          </CarouselItem>
         </Carousel>
       </div>
-      </session>
+    
   )
 }
--- src/pages/teachers.js
+++ src/pages/teachers.js
@@ -1,21 +1,11 @@
 import React from "react"
 import Header from "../components/header/header"
 import Carrusele from "../components/carrusel/carrusele"
-
 export default function teachers() {
   return (
-    <div className="container">
-      <div className="row row-cols-1">
+    <div >
         <Header />
-      </div>
-      <br />
-      <div className="row row-cols-1">
         <Carrusele />
-      </div>
-      <br />
-      <div className="row row-cols-1">
-        cuerpo
-      </div>
     </div>
   )
 }
