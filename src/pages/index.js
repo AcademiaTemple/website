0c77bf07215db3f340fbd71e27b90fc0cd586e98
GerardoQ <gerardoquispe65@gmail.com>
2020-10-16 11:45:56 -0500

    añadiendo calendario :)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header/header"
-
+import CalendarA from "../components/calendar/CalendarA"
 export default function Home() {
   return <div>
     <Header />
     <h1>Academia Temple</h1>
     <p>Mejorando la educación a nivel global</p>
+    <CalendarA></CalendarA>
     <Link to="/about">Entérate más sobre nosotros</Link>
   </div>
 }
