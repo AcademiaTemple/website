5dae0b459f51d75cd1d62b5946ce500db3b21fd2
anarquia17 <jorgdavillara@gmail.com>
2020-10-16 18:16:08 -0500

    feat: v.202010161815 Page Teachers

--- src/pages/teachers.js
+++ src/pages/teachers.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "../components/header/header"
 import Carrusele from "../components/carrusel/carrusele"
 import Card_Teacher from "../components/card_teacher/card_teacher"
+import foto from "../img/Banner4.jpg"
 export default function teachers() {
   return (
     <div >
@@ -9,26 +10,32 @@ export default function teachers() {
         <Carrusele />
         <div className="teacher-container">
         <Card_Teacher
+          rutafoto={foto}
           name="Juan Perez"
           description= "Docente del curso de Angular React Vuejs"
         />
         <Card_Teacher
+          rutafoto={foto}
           name="Vickie Schamberger"
           description= "Docente del curso de Angular React Vuejs"
         />
         <Card_Teacher
+          rutafoto={foto}
           name="Gage Waelchi"
           description= "Docente del curso de Angular React Vuejs"
         />
         <Card_Teacher
+          rutafoto={foto}
           name="Ronaldo Waelchi"
           description= "Docente del curso de Angular React Vuejs"
         />
         <Card_Teacher
+          rutafoto={foto}
           name="Jett Waelchi"
           description= "Docente del curso de Angular React Vuejs"
         />
         <Card_Teacher
+          rutafoto={foto}
           name="Ron Waelchi"
           description= "Docente del curso de Angular React Vuejs"
         />
