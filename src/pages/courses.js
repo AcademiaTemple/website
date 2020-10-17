29f2ae881b08120d10875a90387ddbfedfc34792
Enrique David Avalos Ortega <avalosenator@gmail.com>
2020-10-16 21:41:32 -0500

    Se agrega la barra de navegación en el About, Index, Courses

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Card from "../components/card/card"
 import Header from "../components/header/header"
+import Navbar from "../components/navbar/navbar"
 
 export default function Courses() {
   return (
     <div>
+      <Navbar />
       <Header />
       <h1>Cursos disponibles</h1>
       <div className="courses-container">
