cc40f3e86148c221845ecfe3af7161957dc927ae
anarquia17 <jorgdavillara@gmail.com>
2020-10-22 23:33:19 -0500

    feat: Se restructura cards Teachers

---
+++ src/components/card_teacher/card_teacher_data.jsx
@@ -0,0 +1,12 @@
+export const teachersList = [
+    { 
+        rutafoto:'../img/Profe1.jpg',
+        name: 'Carol Rodri G.', 
+        description: 'Curso de React'
+    },
+    { 
+        rutafoto:'../img/Profe1.jpg',
+        name: 'Carol Rodri G.', 
+        descripcion: 'Curso de React'
+    }
+]
---
+++ src/img/teachers_files/commons.3a32b1bb422c409b0881.hot-update.js
@@ -0,0 +1,23 @@
+webpackHotUpdate("commons",{
+
+/***/ "./node_modules/css-loader/index.js?!./node_modules/postcss-loader/src/index.js?!./src/components/card_teacher/card_teacher.css":
+/*!************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader??ref--12-oneOf-1-1!./node_modules/postcss-loader/src??postcss-2!./src/components/card_teacher/card_teacher.css ***!
+  \************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(true);
+// imports
+
+
+// module
+exports.push([module.i, "h2 {\n    font-family: 'Montserrat', sans-serif;\n    size: 24px;\n}\np {\n    font-family: 'Roboto', sans-serif;\n    size: 18px;\n}\n.btn-outline-info {\n    padding: 10px;\n}", "", {"version":3,"sources":["/home/anarquia17/Documents/cursos/Git y GitHub/website/src/components/card_teacher/card_teacher.css"],"names":[],"mappings":"AAAA;IACI,sCAAqC;IACrC,WAAU;CACd;AACA;IACI,kCAAiC;IACjC,WAAU;CACd;AACA;IACI,cAAa;CACjB","file":"card_teacher.css","sourcesContent":["h2 {\n    font-family: 'Montserrat', sans-serif;\n    size: 24px;\n}\np {\n    font-family: 'Roboto', sans-serif;\n    size: 18px;\n}\n.btn-outline-info {\n    padding: 10px;\n}"],"sourceRoot":""}]);
+
+// exports
+
+
+/***/ })
+
+})
+//# sourceMappingURL=commons.3a32b1bb422c409b0881.hot-update.js.map
