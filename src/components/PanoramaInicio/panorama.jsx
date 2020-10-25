11a93552b705e0181810779bed891fa5c6c9fe61
Enrique David Avalos Ortega <avalosenator@gmail.com>
2020-10-25 00:35:28 -0500

    Ajuste para index responsive componentes Calendar/Panorama

--- src/components/PanoramaInicio/panorama.jsx
+++ src/components/PanoramaInicio/panorama.jsx
@@ -5,12 +5,20 @@ import "./panorama.css"
 export default function Header() {
   return (
     <div className="position-relative bg-img">
-      <div className="position-absolute ctn-text">
-        <h2>El secreto para salir adelante es simplemente empezar</h2>
-        <div className="d-flex justify-content-end">
-          <button type="button" className="boton">
-            <Link to="/courses">Cursos</Link>
-          </button>
+      <div className="container">
+        <div className="row ">
+          <div className="col text-right">
+            <div className="ctn-text">
+              <h2>El secreto para salir adelante <br/> es simplemente empezar.</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col text-right">
+            <button type="button" className="boton">
+              <Link to="/courses">Cursos</Link>
+            </button>
+          </div>
         </div>
       </div>
     </div>
