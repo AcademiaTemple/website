c7fd5600bef0e9ae623c85735f5e5048a4b2b0b5
gricardov <gricardov@gmail.com>
2021-01-18 01:14:18 -0500

    inicio conexión fbase

--- src/api.js
+++ src/api.js
@@ -1,27 +1,28 @@
+import firebase from './firebase';
 import { cursos } from './components/cursos/cursos-data';
 import { docentes } from './components/docentes/docentes-data';
 import { episodios } from './components/episodio/episodios-data';
 
-const obtProfesorCurso = async(idProfesor) => {
+const obtProfesorCurso = async (idProfesor) => {
     const { id, img, nombres, apellidos, sobreMi } = docentes.find(docente => docente.id === idProfesor) || {};
     return { id, img, nombres, apellidos, sobreMi };
 }
 
-const obtCursosPorProfesor = async(idProfesor) => {
+const obtCursosPorProfesor = async (idProfesor) => {
     const resultado = cursos.filter(curso => curso.idProfesor === idProfesor) || [];
     return resultado.map(({ id, titulo, urlImg, descBreve }) => ({ id, titulo, urlImg, descBreve }));
 }
 
-export const obtCursosMuestra = async() => {
+export const obtCursosMuestra = async () => {
     return cursos.map(({ id, titulo, urlImg, descBreve }) => ({ id, titulo, urlImg, descBreve }));
 }
 
-export const obtClasesCurso = async(idCurso) => {
+export const obtClasesCurso = async (idCurso) => {
     const resultado = episodios.find(episodio => episodio.idCurso === idCurso);
     return resultado ? resultado.lista : [];
 }
 
-export const obtCursoExtendido = async(idCurso) => {
+export const obtCursoExtendido = async (idCurso) => {
     const { id, idProfesor, titulo, urlImg, dias, hInicioFin, fInicioFin, excepciones, cancelaciones, urlInscripcion, descBreve, descExtendida, objetivo, requisitos } = cursos.find(curso => curso.id === idCurso) || {};
     return {
         id,
@@ -42,8 +43,22 @@ export const obtCursoExtendido = async(idCurso) => {
     };
 }
 
-export const obtCursosCalendario = async() => {
-    return cursos.map(({ id, titulo, dias, hInicioFin, fInicioFin, excepciones, cancelaciones, urlInscripcion, }) => ({
+export const obtCursosCalendario = async () => {
+
+    let lista = [];
+
+    firebase.firestore().collection('cursos').get().then(qsn => {
+        qsn.forEach(doc => {
+            const { id, titulo, dias, hInicioFin, fInicioFin, excepciones, cancelaciones, urlInscripcion } = doc.data();
+            lista.push({ id, titulo, dias, hInicioFin, fInicioFin, excepciones, cancelaciones, urlInscripcion });
+        });
+    }).catch(error => {
+        console.log(error);
+        lista = [];
+    });
+
+    return lista;
+    /*return cursos.map(({ id, titulo, dias, hInicioFin, fInicioFin, excepciones, cancelaciones, urlInscripcion, }) => ({
         id,
         titulo,
         dias,
@@ -52,10 +67,11 @@ export const obtCursosCalendario = async() => {
         excepciones,
         cancelaciones,
         urlInscripcion,
-    }));
+    }));*/
+
 }
 
-export const obtProfesores = async() => {
+export const obtProfesores = async () => {
     const filtrados = docentes.map(({ id, img, nombres, apellidos, sobreMi, etiquetas, likes, redes }) => ({
         id,
         img,
